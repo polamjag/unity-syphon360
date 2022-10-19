1735489787b808dfd0509f210a806d640da616ae
polamjag <s@polamjag.info>
2022-10-20 00:32:21 +0900

    use SimpleFileBrowser

--- Assets/MoviePlayerController.cs
+++ Assets/MoviePlayerController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
+using SimpleFileBrowser;
 
 public class MoviePlayerController : MonoBehaviour
 {
@@ -10,6 +10,7 @@ public class MoviePlayerController : MonoBehaviour
     {
         var player = GameObject.Find("Movie360");
         player.GetComponent<UnityEngine.Video.VideoPlayer>().url = "/Users/satoru/Desktop/ayutthaya_-_easy_tripod_paint___360_vr_master_series___free_download (1080p).mp4";
+        FileBrowser.SetFilters(true, new FileBrowser.Filter("Movies", ".mov", ".mp4"));
     }
 
     // Update is called once per frame
@@ -17,11 +18,13 @@ public class MoviePlayerController : MonoBehaviour
     {
         if (Input.GetKey("p"))
         {
-            var player = GameObject.Find("Movie360");
-            player.SetActive(false);
-            string path = EditorUtility.OpenFilePanel("Overwrite with mp4", "", "mov,mp4,insv");
-            player.GetComponent<UnityEngine.Video.VideoPlayer>().url = path;
-            player.SetActive(true);
-        }
+			FileBrowser.ShowLoadDialog((paths) => {
+				if (paths[0] != null)
+                {
+					var player = GameObject.Find("Movie360");
+					player.GetComponent<UnityEngine.Video.VideoPlayer>().url = paths[0];
+				}
+			}, () => {}, FileBrowser.PickMode.Files, false, null, null, "Load Movie", "Load");
+		}
     }
 }
